bb14ecf9cfc03dbe27f54a368cdc96389560ceb6
Rokas Urbonavičius <rksurbonavicius@gmail.com>
2025-11-14 22:07:15 +0100

    Use the correct state in cubit

--- lib/features/news_list/4_features/news_details/1_presentation/cubits/news_details_cubit.dart
+++ lib/features/news_list/4_features/news_details/1_presentation/cubits/news_details_cubit.dart
@@ -1,8 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:news_app/features/news_list/1_presentation/cubits/news_list_state.dart';
 import 'package:news_app/features/news_list/2_domain/entities/article.dart';
+import 'package:news_app/features/news_list/4_features/news_details/1_presentation/cubits/news_details_state.dart';
 
-class NewsDetailsCubit extends Cubit<NewsListState> {
-
-  NewsDetailsCubit({required Article article}) : super(const NewsListInitial());
+class NewsDetailsCubit extends Cubit<NewsDetailsState> {
+  NewsDetailsCubit({required Article article}) : super(NewsDetailsInitial(article));
 }
